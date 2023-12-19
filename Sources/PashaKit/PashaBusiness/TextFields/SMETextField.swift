8aacf1646a1794455d67533cb507e58bb6005cac
Farid Valiyev <farid.valiyev@pashabank.az>
2023-12-19 16:46:31 +0400

    custom_placeholder

--- Sources/PashaKit/PashaBusiness/TextFields/SMETextField.swift
+++ Sources/PashaKit/PashaBusiness/TextFields/SMETextField.swift
@@ -889,6 +889,7 @@ public class SMETextField: UIView {
             self.performAnimation { [weak self] in
                 guard let self = self else { return }
 //                self.customPlaceholder.textColor = self.errorStateColor
+                self.customPlaceholder.textColor = self.placeholderTextColor
                 self.customBorder.layer.borderColor = self.errorStateColor.cgColor
                 self.textFieldStack.updateExistingBottomBorderThickness(to: 1.0)
                 self.textFieldStack.updateExistingBottomBorderColor(to: self.errorStateColor)
