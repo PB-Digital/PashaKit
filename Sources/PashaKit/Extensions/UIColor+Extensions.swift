1717aa14bc7a7b1283e67733a5a4325b117d22cb
Farid Valiyev <farid.valiyev@pashabank.az>
2024-01-10 11:23:54 +0400

    color fixes

--- Sources/PashaKit/Extensions/UIColor+Extensions.swift
+++ Sources/PashaKit/Extensions/UIColor+Extensions.swift
@@ -182,8 +182,8 @@ extension UIColor {
         static var SMETextFieldText: UIColor {
             return UIColor { (traits) -> UIColor in
                 return traits.userInterfaceStyle == .dark ?
-                UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1) :
-                UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1)
+                UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1):
+                UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1)
             }
         }
         
